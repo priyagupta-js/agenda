177beeef9e6611298b6a97f48c6fb64368bd3f09
Priya Gupta <priyagupta052k@gmail.com>
2024-09-20 18:48:31 +0530

    Add and display list

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-let results = document.getElementById("results");
+let text = document.querySelector("#text");
+let S_button = document.querySelector("#btn-submit");
+let results = document.querySelector("#D_results");
+
+
+S_button.addEventListener("click",displayList);
+
+function displayList(event)
+{
+    event.preventDefault();
+    message = text.value;
+    // results.innerHTML = message;
+    let Li = document.createElement("li");
+    let textNode = document.createTextNode(message);
+    Li.appendChild(textNode);
+    if (message == " ")
+    {
+        alert("You must write something");
+    }
+    else
+    {
+        results.appendChild(Li);
+    }
+    text.value=" ";
+    // LiElement.style.listStyleType = "circle";
+    
+}
+
+
